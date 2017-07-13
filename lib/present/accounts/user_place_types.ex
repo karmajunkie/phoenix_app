75fcb1c15dc4ce514d0b5dd668546377aedfd437
Romain Séguy <rom.seguy@gmail.com>
2017-07-13 18:38:32 +0200

    karmajunkie suggestion

--- lib/present/accounts/user_place_types.ex
+++ lib/present/accounts/user_place_types.ex
@@ -1,10 +1,17 @@
 defmodule Present.Accounts.UserPlace.Types do
   use Absinthe.Schema.Notation
   use Absinthe.Ecto, repo: Present.Repo
+  import Timex
+
+  scalar :timestamptz do
+    parse &Timex.parse(&1.value, "{ISOz}")
+    serialize &Timex.format!(&1, "{ISOz}")
+  end
 
   object :user_place do
     field :id, :id
     field :user_id, :id
     field :place_id, :id
+    field :inserted_at, :timestamptz
   end
 end
